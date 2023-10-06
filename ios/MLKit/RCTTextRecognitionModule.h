fdb12ea751b2381e6a5d79f9bbd2079ffae95b77
HieuNguyen <hieunguyenbk93@gmail.com>
2023-10-06 13:48:00 +0700

    text recognition ios

---
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTTextRecognitionModule.h
+//  NbhTestProject
+//
+//  Created by Phòng Giải Pháp on 06/10/2023.
+//
+
+#ifndef RCTTextRecognitionModule_h
+#define RCTTextRecognitionModule_h
+
+
+#endif /* RCTTextRecognitionModule_h */
---
+++ ios/MLKit/RCTTextRecognitionModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTTextRecognitionModule.m
+//  NbhTestProject
+//
+//  Created by Phòng Giải Pháp on 06/10/2023.
+//
+
+#import <Foundation/Foundation.h>
