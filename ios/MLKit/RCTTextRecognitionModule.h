c220b78b2d39e2920ca255e8943c9e624acd07c5
HieuNguyen <hieunguyenbk93@gmail.com>
2023-10-06 13:48:14 +0700

    text recognition ios

--- ios/MLKit/RCTTextRecognitionModule.h
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -8,5 +8,10 @@
 #ifndef RCTTextRecognitionModule_h
 #define RCTTextRecognitionModule_h
 
+#import <React/RCTBridgeModule.h>
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+
+@end
+
 
 #endif /* RCTTextRecognitionModule_h */
